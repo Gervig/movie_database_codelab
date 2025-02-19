8c2718131ee3e60b63b4f99eba1fc3ae331e7696
RikkeMariussen <cph-rm225@cphbusiness.dk>
2025-02-19 12:30:24 +0100

    getDataFromClientWithTitleAndYear

--- src/main/java/app/ApiReader.java
+++ src/main/java/app/ApiReader.java
@@ -12,6 +12,7 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
@@ -83,22 +84,59 @@ public class ApiReader
         return null;
     }
 
-    public List<MovieDTO> getMovieData(String json) {
+
+    public String getDataFromClientWithTitleAndYear(String url, String title, String releaseYear)
+    {
+        String movieURL = url.replace("%%", title);
+        movieURL = movieURL.replace(" ", "%20");
+        movieURL = movieURL.replace("##", releaseYear);
+
+        try
+        {
+            HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .uri(new URI(movieURL))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200)
+            {
+                String body = response.body();
+                return body;
+            } else
+            {
+                System.out.println("GET request failed. Status code: " + response.statusCode());
+            }
+
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
+    public List<MovieDTO> getMovieData(String json)
+    {
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         objectMapper.registerModule(new JavaTimeModule());
 
-        try {
+        try
+        {
             // Map the 'results' array from the JSON response to a List of MovieDTOs
             return Arrays.asList(objectMapper.readValue(
                     objectMapper.readTree(json).get("results").toString(), MovieDTO[].class
             ));
 
-        } catch (JsonProcessingException jPE) {
+        } catch (JsonProcessingException jPE)
+        {
             jPE.printStackTrace();
         }
         return null;
     }
-
 
 
 }
--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -16,13 +16,20 @@ public class Main
         String api_key = System.getenv("api_key");
 
         String movie = "Lord of the rings";
+        String releaseYear = "2001";
         String response = apiReader.getDataFromClientWithTitle("https://api.themoviedb.org/3/search/movie?query=%%&api_key=" + api_key, movie);
+        String response2 = apiReader.getDataFromClientWithTitleAndYear("https://api.themoviedb.org/3/search/movie?query=%%&primary_release_year=##&api_key="+api_key, movie, releaseYear);
 
-        System.out.println(response);
+        //System.out.println(response);
+        //System.out.println(response2);
+
 
         List<MovieDTO> movieDTO = apiReader.getMovieData(response);
-        
-        movieDTO.stream().forEach(System.out::println);
+        //  movieDTO.stream().forEach(System.out::println);
+
+        List<MovieDTO> movieDTO2 = apiReader.getMovieData(response2);
+        movieDTO2.stream().forEach(System.out::println);
+
 
     }
 

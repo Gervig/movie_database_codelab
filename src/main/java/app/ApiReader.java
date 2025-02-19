f21a68d8ee8985cdb80ca0a2b57ead95b1344f93
RikkeMariussen <cph-rm225@cphbusiness.dk>
2025-02-19 10:38:31 +0100

    make getDataFromClientWithTitle

--- src/main/java/app/ApiReader.java
+++ src/main/java/app/ApiReader.java
@@ -37,4 +37,34 @@ public class ApiReader
         return null;
     }
 
+    public String getDataFromClientWithTitle(String url, String title){
+        String movieURL = url.replace("%%", title);
+        movieURL = url.replace(" ","%20");
+
+        try
+        {
+            HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .version(HttpClient.Version.HTTP_1_1)
+                    .uri(new URI(movieURL))
+                    .GET()
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            if (response.statusCode() == 200)
+            {
+                String body = response.body();
+                return body;
+            } else {
+                System.out.println("GET request failed. Status code: " + response.statusCode());
+            }
+
+        } catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
